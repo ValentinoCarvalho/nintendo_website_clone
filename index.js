555a10520266d11ada2f724add1472c07bf78883
ValentinoCarvalho <valentinomatoscarvalho@gmail.com>
2026-04-16 01:47:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const greatCarousel = document.getElementById('mainCarousel');
 
+const mainCarouselRadio = document.getElementsByClassName('mainCarouselRadio')
+
+var viwerWidth = window.innerWidth;
+
 function greatCarouselMove() {
 
-    setTimeout(() => { greatCarousel.scrollTo({ left: 2000 }) }, 5000)
+    mainCarouselRadio[0].checked = true;
 
-    setTimeout(() => { greatCarousel.scrollTo({ left: 3000 }) }, 10000)
+    setTimeout(() => { greatCarousel.scrollTo({ left: viwerWidth }); viwerWidth = window.innerWidth; mainCarouselRadio[1].checked = true; }, 5000)
 
-    setTimeout(() => { greatCarousel.scrollTo({ left: 50000 }) }, 15000)
+    setTimeout(() => { greatCarousel.scrollTo({ left: viwerWidth * 2 }); viwerWidth = window.innerWidth; mainCarouselRadio[2].checked = true; }, 10000)
 
-    setTimeout(() => { greatCarousel.scrollTo({ left: 0 }); greatCarouselMove() }, 20000)
+    setTimeout(() => { greatCarousel.scrollTo({ left: viwerWidth * 3 }); viwerWidth = window.innerWidth; mainCarouselRadio[3].checked = true; }, 15000)
+
+    setTimeout(() => { greatCarousel.scrollTo({ left: 0 }); viwerWidth = window.innerWidth; greatCarouselMove() }, 20000)
 
 }
 
-greatCarouselMove()
+greatCarouselMove();
+
 
 
 
